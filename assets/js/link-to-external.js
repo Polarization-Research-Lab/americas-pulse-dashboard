2ee3f1832c965bb408a1a4dadc9603d36c83e070
m <m@ms-MacBook-Pro.local>
2023-05-08 17:50:25 -0400

    upd

--- assets/js/link-to-external.js
+++ assets/js/link-to-external.js
@@ -1,3 +1,4 @@
+// shamelessly pulled from jhvanderschee @ https://jekyllcodex.org/without-plugin/new-window-fix/#
 function external_new_window() {
     for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
         var b = c[a];
@@ -10,5 +11,19 @@ function external_new_window() {
     }
 }
 external_new_window()
-// shamelessly pulled from jhvanderschee @ https://jekyllcodex.org/without-plugin/new-window-fix/#
 
+function pdf_new_window ()
+{
+    if (!document.getElementsByTagName) return false;
+    var links = document.getElementsByTagName("a");
+    for (var eleLink=0; eleLink < links.length; eleLink ++) {
+    if ((links[eleLink].href.indexOf('.pdf') !== -1)||(links[eleLink].href.indexOf('.doc') !== -1)||(links[eleLink].href.indexOf('.docx') !== -1)) {
+        links[eleLink].onclick =
+        function() {
+            window.open(this.href);
+            return false;
+        }
+    }
+    }
+} 
+external_new_window();
