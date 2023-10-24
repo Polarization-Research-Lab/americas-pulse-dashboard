92d21e91878620f3871669c934e6f5ede6acda04
seanjw <seanjwestwood@gmail.com>
2023-10-24 02:49:11 +0000

    routine update

--- .build/pull_data.py
+++ .build/pull_data.py
@@ -2,10 +2,10 @@ import sys
 
 import pandas as pd
 
-sys.path.append("../../../utils/")
+sys.path.append("../../../.utils/")
 import distilldb as ddb
 
-db = ddb.Database(config="secrets.json")
+db = ddb.Database(config="../../../../.secrets/db.json")
 
 with db.Session() as session:
     pd.read_sql(
