a6dddff79bf44241f87b9de47fe976203d3694bd
Matt <matw7r@gmail.com>
2023-11-13 20:49:01 +0000

    routine update

--- .build/pull_download_links.py
+++ .build/pull_download_links.py
@@ -1,27 +1,33 @@
-import sys
+import sys, json
 
+# Dependencies
 import pandas as pd
+import ibis
 
-sys.path.append("utils/")
-import distilldb as ddb
+# Setup
+with open('../../../../.secrets/db.json', 'r') as file:
+    secrets = json.load(file)
 
-db = ddb.Database(config="../../../../.secrets/db.json", database = "web")
+## Connect to new elite database with updated users
+conn = ibis.mysql.connect(
+    host = secrets['host'],
+    user = secrets['username'],
+    password = secrets['password'],
+    database = 'web',
+)
+links = conn.table("download_counts").execute()
 
-with db.Session() as session:
-    links = pd.read_sql(
-        session.query(db["download_counts"]).statement,
-        db.engine,
-    )
-    links = links[links["file"].str.contains("week")]
-    links["week"] = links["file"].apply(
-        lambda x: int(x.replace('downloads/citizens/','').split("week")[-1].split("_")[0])
-    )
-    links["year"] = links["file"].apply(lambda x: int(x.split("_")[0].split("/")[-1]))
+# Get links and paste them to a data file for jekyll
+links = links[links["file"].str.contains("week")]
+links["week"] = links["file"].apply(
+    lambda x: int(x.replace('downloads/citizens/','').split("week")[-1].split("_")[0])
+)
+links["year"] = links["file"].apply(lambda x: int(x.split("_")[0].split("/")[-1]))
 
-    links = links.sort_values(["year", "week"], ascending=False)
+links = links.sort_values(["year", "week"], ascending=False)
 
-    # print(links)
-    links[["year", "week", "file"]].to_json(
-        "../_data/datalinks.json",
-        orient="records",
-    )
+# print(links)
+links[["year", "week", "file"]].to_json(
+    "../_data/datalinks.json",
+    orient="records",
+)
--- assets/js/download-btn.js
+++ assets/js/download-btn.js
@@ -1,6 +1,6 @@
 var btn = document.getElementById('download-btn-all')
 btn.onclick = function () {
-    window.location.href = 'https://apps.polarizationresearchlab.org/api/count/downloads/citizens/all_data.zip';
+    window.location.href = 'https://apps.polarizationresearchlab.org/api/count/downloads/citizens/all-data.zip';
 }
 
 var btn = document.getElementById('download-btn-week')
@@ -17,5 +17,6 @@ btn.onclick = function () {
     var file = 'downloads/citizens/toplines/' + selected.dataset.year + '_week' + selected.dataset.week + '.pdf'
 
     // window.location.href = 'https://dtwknrym6jvxy.cloudfront.net/' + file;
-    window.open('https://d3ideup5qjydvl.cloudfront.net/' + file);
+    // window.open('https://d3ideup5qjydvl.cloudfront.net/' + file); // <-- new
+    window.open('https://d2goaxv8lbgkdm.cloudfront.net/' + file); // <-- old
 }
